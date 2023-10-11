fc8eb7e3085cba354e36d3f71eeac0034a617b5e
Zara Martín <zmartand@myuax.es>
2023-10-11 20:04:27 +0200

    Renderizador.java hilos para mejorar eficiencia

--- src/main/java/org/example/Renderizador.java
+++ src/main/java/org/example/Renderizador.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Renderizador {
+    private final int width,height;
+    private final int maxIterations = 1000;
+
+    public Renderizador(int width,int height){
+        this.width = width;
+        this.height = height;
+    }
+    public Color[][] render(int numProcesos){
+        ExecutorService executorService = Executors.newFixedThreadPool(numProcesos);
+        int segmentHeight = height / numProcesos;
+        List<Future<List<Color>>> futures = new ArrayList<>();
+
+        for (int i = 0;i < numProcesos;i++){
+            int startY = i * segmentHeight;
+            int endY = (i + 1) * segmentHeight;
+            Callable<List<Color>> task = new Calculador(startY,endY,width,height,maxIterations);
+            Future<List<Color>> future = executorService.submit(task);
+            futures.add(future);
+        }
+        Color[][] image = new Color[width][height];
+        for (int i=0; i<numProcesos; i++) {
+            try {
+                List<Color> segmentColors = futures.get(i).get();
+                for (int y = i * segmentHeight; y < (i + 1) * segmentHeight; y++) {
+                    for (int x = 0; x < width; x++) {
+                        image[x][y] = segmentColors.get((y - i * segmentHeight) * width + x);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        executorService.shutdown();
+        return image;
+    }
 }
