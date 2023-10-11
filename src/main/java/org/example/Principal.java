1abd7efa77a90401afdbd57a3d9dd985bb57eada
Zara Martín <zmartand@myuax.es>
2023-10-11 20:18:08 +0200

    Clase principal mejora preguntar num trabajadores

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -7,14 +7,16 @@ public class Principal {
     public void generarConjuntoMandelbrot(){
         int width = 800;
         int height = 600;
-        int numProcesos = 4;
-        Renderizador renderizador = new Renderizador(width,height);
+
+        //Pregunta al usuario cuántos procesos desea utilizar.
+        String input = JOptionPane.showInputDialog("Ingrese el número de procesos:");
+        int numProcesos = Integer.parseInt(input);
+        Renderizador renderizador = new Renderizador(width, height);
         Color[][] image = renderizador.render(numProcesos);
-        JFrame frame = new JFrame("Mandelbrot");
+        JFrame frame = new JFrame("Conjunto de Mandelbrot");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Panel(image));
         frame.setSize(width, height);
-        frame.setLocationRelativeTo(null);
+        frame.add(new Panel(image));
         frame.setVisible(true);
     }
 }
