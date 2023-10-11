6ed029c5473be9516ec9fc371e9998034e5efdaf
Zara Martín <zmartand@myuax.es>
2023-10-11 19:42:53 +0200

    constructor de la clase Calculador.java

--- src/main/java/org/example/Calculador.java
+++ src/main/java/org/example/Calculador.java
@@ -3,5 +3,15 @@ package org.example;
 import java.awt.*;
 
 public class Calculador implements Callable<List<Color>>{
+    private final int startY, endY, width, height, maxIterations;
+
+    public Calculador(int startY, int endY, int width, int height, int maxIterations) {
+        this.startY = startY;
+        this.endY = endY;
+        this.width = width;
+        this.height = height;
+        this.maxIterations = maxIterations;
+    }
+    
 
 }
